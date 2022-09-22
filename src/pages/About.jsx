4c37e3ca8d6d627b9252f6b0e1d0014e419b9159
Naelia Salas <naeliasalas@gmail.com>
2022-09-22 10:54:31 +0200

    First commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,23 @@
+
+function About() {
+  return (
+    <div>
+        <h1 className="text-6xl mb-4">Github Finder</h1>
+        <p className="mb-4 text-2xl font-light"> 
+        A react app to search Github progiles and see progile detials. This project is part of the
+        <a href="https://www.udemy.com/course/modern-react-front-to-back/">
+            {" "}
+            React fron to back
+        </a> {" "}
+        Udemy course by
+        <strong>
+            <a href="https://traversymedia.com"> Naelia</a>
+        </strong>
+
+
+        </p>
+    </div>
+  )
+}
+
+export default About
